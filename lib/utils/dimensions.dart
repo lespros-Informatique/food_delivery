d97e6b585a30635256a2cb6772972d3f63ea9399
mac <mac@192.168.1.6>
2026-01-07 18:22:34 +0000

    Initial commit after zip download

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,52 @@
+import 'package:get/get.dart';
+class Dimensions {
+  // 1 screen height = 844
+  static double screenHeight = Get.context!.height;
+
+  static double screenWidth = Get.context!.width;
+
+// for page view height
+  static double pageView = screenHeight / 2.64;
+
+  static double pageViewContainer = screenHeight / 3.84;
+
+  static double pageViewTextContainer = screenHeight / 7.03;
+
+// for height and width padding and margin
+  static double height10 = screenHeight / 84.4;
+
+  static double height15 = screenHeight / 56.27;
+
+  static double height20 = screenHeight / 42.2;
+
+  static double height30 = screenHeight / 28.13;
+
+  static double height45 = screenHeight / 18.76;
+
+//width
+  static double width10 = screenHeight / 84.4;
+
+  static double width15 = screenHeight / 56.27;
+
+  static double width20 = screenHeight / 42.2;
+
+  static double width30 = screenHeight / 28.13;
+
+//radius
+  static double radius15 = screenHeight / 56.27;
+  static double radius20 = screenHeight / 42.2;
+
+  static double radius30 = screenHeight / 28.13;
+
+//font size
+  static double font20 = screenHeight / 42.2;
+
+  static double font26 = screenHeight / 32.46;
+
+  static double font16 = screenHeight / 52.75;
+
+//icon size
+  static double iconSize24 = screenHeight / 35.17;
+
+  static double iconSize16 = screenHeight / 52.75;
+}
